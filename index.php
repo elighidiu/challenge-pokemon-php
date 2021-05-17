f44b04da6165c83832e122799a3109ae358a2134
Your Nameeli <elighidiu@yahoo.com>
2021-05-17 15:53:16 +0200

    isset function & second API

--- index.php
+++ index.php
@@ -13,24 +13,46 @@
     
 
     <?php
-        $name = $_GET['name'];
-   
-        $api_url = "https://pokeapi.co/api/v2/pokemon/$name";
+        //isset function — will determine if a variable is declared and is different than null
+       if(isset($_GET['name'])){ 
+            $name = strtolower($_GET['name']); // strtoupper will convert text to lowercase
+            $api_url = "https://pokeapi.co/api/v2/pokemon/$name";
+            $api_species = "https://pokeapi.co/api/v2/pokemon-species/$name"; 
+       } else {
+            $api_url = "https://pokeapi.co/api/v2/pokemon/1";
+            $api_species = "https://pokeapi.co/api/v2/pokemon-species/1";
+       }
+
         $json_data = file_get_contents($api_url); // Read JSON file
+        $json_data_species = file_get_contents($api_species); // Read JSON file
+
         $response_data = json_decode($json_data); // Decode JSON data into PHP array
+        $response_data_species = json_decode($json_data_species); // Decode JSON data into PHP array
 
-       // $poke_forms = $response_data->forms; // Info that exists in 'forms'
         $poke_img = $response_data->sprites->front_default;
-        $poke_name = $response_data->forms[0]->name;
+        $poke_name = ucfirst($response_data->forms[0]->name); //ucfirst will write the first letter in capital
+        $poke_move = $response_data->moves;
 
-        echo "name: ".$poke_name;
-        echo "<br />";
-        echo "id: ".$response_data->id;
-        echo "<br />";
+        $species = $response_data_species->evolves_from_species;
+
+        if($species!==NULL){
+            $previous_poke = $response_data_species->evolves_from_species; //get the name of the previous species
+        }
+   
+    ?>
+
+        <?php echo "<h2>".$poke_name." (#".$response_data->id.")</h2>"; ?>
+        <?php echo "<img src='".$poke_img."'>"; ?>
         
-        echo "<img src='".$poke_img."'>";
-               
-    ?> 
+        <ul>
+            <p>Moves</p>
+            <?php for ($i=0; $i<4; $i++){
+                echo "<li>".$poke_move[$i]->move->name."</li>"; //displays the moves in a list
+                //"Move: ".$poke_move[$i]->move->name;   
+                }  
+            ?>
+        </ul>
+ 
 
 </body>
 </html>
